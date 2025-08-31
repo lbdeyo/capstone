9aa06d96a09c274a43006b029017728c8e1b5bfe
lbdeyo <lbdeyo@gmail.com>
2025-08-31 17:04:18 -0500

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
+import Testimonials from "./components/Testimonials";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <Nav />
+      </header>
+
+      <main>
+        <Hero />
+        <Specials />
+        <Testimonials />
+        <About />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+export default function Nav() {
+  return (
+    <nav>
+      <a href="/" role="button">
+        Home
+      </a>
+      <a href="/" role="button">
+        Home
+      </a>
+      <a href="/" role="button">
+        Home
+      </a>
+      <a href="/" role="button">
+        Home
+      </a>
+    </nav>
+  );
+}
