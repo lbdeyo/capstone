774365d7751acf7abecd9a8ad542f27d01fcaf22
lbdeyo <lbdeyo@gmail.com>
2025-09-01 16:18:04 -0500

    Adding images and styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,22 @@
 export default function Hero() {
   return (
     <section className="hero">
-      <h1>Howdy</h1>
+      <section className="heroLeft">
+        <h1>Little Lemon</h1>
+        <h3>Chicago</h3>
+        <p>
+          We are a family owned Mediterranean restaurant, focused on traditional
+          recipes served with a modern twist.
+        </p>
+
+        <a href="/reservations" className="button">
+          Reserve a Table
+        </a>
+      </section>
+      <div>
+        {" "}
+        <img src="/img/hero-image.png" alt="restaurant food" />
+      </div>
     </section>
   );
 }
--- src/components/Specials.jsx
+++ src/components/Specials.jsx
@@ -1,3 +1,38 @@
 export default function Specials() {
-  return <section></section>;
+  return (
+    <section className="specials">
+      <section className="specialsHeader">
+        <h2>This week's specials!</h2>
+        <a href="/reservations" className="button">
+          Reserve a Table
+        </a>
+      </section>
+      <section className="cards">
+        <div className="card">
+          <h4>Greek Salad</h4>
+          <p>
+            The famous greek salad of crispy lettuce, peppers, olives and our
+            Chicago style feta cheese, garnished with crunchy garlic and
+            rosemary croutons.
+          </p>
+        </div>
+        <div className="card">
+          <h4>Greek Salad</h4>
+          <p>
+            The famous greek salad of crispy lettuce, peppers, olives and our
+            Chicago style feta cheese, garnished with crunchy garlic and
+            rosemary croutons.
+          </p>
+        </div>
+        <div className="card">
+          <h4>Greek Salad</h4>
+          <p>
+            The famous greek salad of crispy lettuce, peppers, olives and our
+            Chicago style feta cheese, garnished with crunchy garlic and
+            rosemary croutons.
+          </p>
+        </div>
+      </section>
+    </section>
+  );
 }
